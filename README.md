376e9316df0b8cbc9a5d88eddcdc6478dc78d882
unknown <ballig360@gmail.com>
2025-02-16 02:07:05 +0530

    added login data to store

--- README.md
+++ README.md
@@ -18,3 +18,16 @@
     Route=/login => login
     Route=/connections => Connections
     Route=/profile => Profile
+
+
+- creating login page
+- install axios
+- cors error making api call form x domain to y domain,cross origin,they are on different domain
+- cors - install cors in backend => add middleware with configurations:origin,credentials:true
+- setting withCredentials true in both frontend and backend
+- whenever you are making api call so pass axios => withCredentials : true,if you dont do it you will not get the token back
+- installing @reduxjs/toolkit and react-redux
+- created/configured appStore,then provided in App.jsx by <Provider>
+- created slice,exported reducer and actions and then added reducer to store.
+- login to see if data coming properly to store
+- refactor our code,to add constants file + create a componets folder.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,23 @@
+import { useSelector } from "react-redux"
 
 const Navbar = () => {
+  //part of store we are subscribing to 
+  const user = useSelector(store => store.user);
+  console.log(user);
   return (
     <><div className="navbar bg-base-300">
     <div className="flex-1">
       <a className="btn btn-ghost text-xl">💻 Connectify</a>
     </div>
-    <div className="flex-none gap-2">
-      <div className="form-control">
-      </div>
-      <div className="dropdown dropdown-end mx-5">
+    {user && (<div className="flex-none gap-2">
+      <div className="form-control">Welcome,{user.firstName}</div>
+      {/* only we can see the photo and profile section when the user is logged in  */}
+      <div className="dropdown dropdown-end mx-5 flex">
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
           <div className="w-10 rounded-full">
             <img
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              alt="user photo"
+              src={user.photoUrl}/>
           </div>
         </div>
         <ul
@@ -29,7 +33,7 @@ const Navbar = () => {
           <li><a>Logout</a></li>
         </ul>
       </div>
-    </div>
+    </div>)}
   </div></>
   )
 }

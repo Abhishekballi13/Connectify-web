991552823d3b786713b18decc4acec5720601e35
unknown <ballig360@gmail.com>
2025-02-20 19:15:07 +0530

    made changes in EditProfile

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,10 +1,9 @@
 import {useState } from "react";
-import UserCard from "./userCard";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
-
+import UserCard from "./UserCard";
 
 
 const EditProfile = ({user}) => {

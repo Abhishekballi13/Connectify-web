7331804a97de6f738f63edde918d6ac2ef2cb77e
unknown <ballig360@gmail.com>
2025-02-24 02:58:49 +0530

    for prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
 
 //local
-export const BASE_URL = "http://localhost:7777";
+// export const BASE_URL = "http://localhost:7777";
 
 //for production in aws
 // export const BASE_URL = "/api";
 
 
 //for production at vercel
-// export const BASE_URL = "https://connectify-murex.vercel.app"
+export const BASE_URL = "https://connectify-murex.vercel.app"

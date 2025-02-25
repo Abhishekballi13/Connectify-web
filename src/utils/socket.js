d80757d69260d36e754a8f27bc8cb4752442f033
unknown <ballig360@gmail.com>
2025-02-25 22:58:15 +0530

    bug fix

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,11 +4,11 @@ import { BASE_URL } from "./constants";
 export const createSocketConnection = () => {
     //basically you are telling here to connect here to backend system.
 
-    // if(location.hostname==="localhost"){
-    //     return io(BASE_URL);
-    // }else{
-    //     return io("/",{path:"/api/socket.io"});
-    // }   
-    //default value of path is /socket.io/
-    return io(BASE_URL);
+    if(location.hostname==="localhost"){
+        return io(BASE_URL);
+    }else{
+        return io("/",{path:BASE_URL+"/socket.io"});
+    }   
+    // default value of path is /socket.io/
+    // return io(BASE_URL);
 }

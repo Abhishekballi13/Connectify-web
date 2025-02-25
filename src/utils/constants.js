42b92650880ed94381c018d3f5a3434fdc262fd8
unknown <ballig360@gmail.com>
2025-02-25 21:33:14 +0530

    changed constant.js according to our mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 
 //local
-export const BASE_URL = "http://localhost:7777";
+// export const BASE_URL = "http://localhost:7777";
 
 //for production in aws
 // export const BASE_URL = "/api";
@@ -9,6 +9,7 @@ export const BASE_URL = "http://localhost:7777";
 //for production at vercel
 // export const BASE_URL = "https://connectify-murex.vercel.app"
 
-// if(location.hostname==="localhost"){
-
-// }
+export const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : "https://connectify-murex.vercel.app";
